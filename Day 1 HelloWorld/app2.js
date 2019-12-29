6de87cf0a254c442414a18300cb4beaaba1a074a
kangjieun99 <51572322+kangjieun99@users.noreply.github.com>
2019-12-29 15:23:04 +0900

    rest api add

--- Day 1 HelloWorld/app2.js
+++ Day 1 HelloWorld/app2.js
@@ -21,3 +21,24 @@ app.get("/home", (req, res) => {
 	let param = req.param("man");
 	res.send(`${user} : ${param}`);
 });
+
+app.get("/api/user", (req, res) =>{
+	let users = {
+		users: [
+			{id: 1, name: "홍길동", age: 25},
+			{id: 2, name: "홍길만", age: 29},
+			{id: 3, name: "홍길용", age: 28}
+		],
+		cnt: 3
+	};
+
+	res.json(users);
+});
+
+app.get("/blog/:category/:id", (req, res) => {
+
+	let category = req.params.category;
+	let id = req.params.id;
+	res.send(`${category} : ${id}`);
+
+})
