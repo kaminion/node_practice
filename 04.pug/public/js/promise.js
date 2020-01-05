7ff4022e936c83df316f3f22d4c034c7042c0e47
kangjieun99 <51572322+kangjieun99@users.noreply.github.com>
2020-01-05 17:20:34 +0900

    20200105 fininish

--- 04.pug/public/js/promise.js
+++ 04.pug/public/js/promise.js
@@ -13,16 +13,19 @@
 // 여기까지가 콜백 원리
 
 // promise model
-function init(){
+async function init(){
 	// then 절안에 있는 콜백메서드가 호출하는 함수의 resolve로 들어감
 	// then은 처리가 된 이후에 실행 할 콜백메서드라고 보면 됨, promise 객체에 접근하기위함
-	var date = getDays().then(({d, a})=>{
-		console.log(d.getDay());
-		console.log(a);
-	});
+	// var date = getDays().then(({d, a})=>{
+	// 	console.log(d.getDay());
+	// 	console.log(a);
+	// });
 
 	// resolve에서 전달한 값을 사용함
 	// Promise 객체만 async await 사용가능 (then절로 인한 콜백메서드 X)
+
+	let a = await getDays();
+	console.log(a);
 }
 
 function getDays(){
@@ -33,7 +36,7 @@ function getDays(){
 	return new Promise(function(resolve, reject){
 		// 콜백함수 실행
 		resolve({a, d});
-		// 리졸브 실행 후 리턴해줌 
+		// 리졸브 실행 후 리턴해줌, await써도 resolve에 있는걸 넘겨준다. resolve로 넘겨주는 값이 최종 리턴문이라고 보면 됨
 	});
 
 }
