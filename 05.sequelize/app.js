53d0f3d1fc74731a894c897cf94ab8e285bea432
declan andrew <anysweetimes@naver.com>
2020-02-02 10:55:07 +0900

    add morgan

--- 05.sequelize/app.js
+++ 05.sequelize/app.js
@@ -1,19 +1,25 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' }) //로그를 파일로 저장하겠다
+app.use(logger('combined', { stream: accessLogStream }))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(logger('dev')); // 모건이 로그를 남김, 얜 없어도 됨 (터미널 전용)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -23,6 +29,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
+
+// 라우터로 들어가지못하면 404 error를 띄워버림
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -30,6 +38,8 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+
+  // 이 메세지는 error.pug에 담겨져있다., response 객체에 전역등록
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
